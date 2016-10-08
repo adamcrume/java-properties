93054fe1decdf5a4b8bf57cca5ef66d69685d45a
Adam Crume <adamcrume@gmail.com>
2016-10-08 14:46:18 -0800

    Update the documentation

---
+++ implementors/encoding/types/trait.RawEncoder.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['encoding'] = [];implementors['encoding'] = [];implementors["encoding"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
